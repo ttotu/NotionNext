95e45a8d06f4b69dc5e476759146765f98912264
ttotu <58546777+ttotu@users.noreply.github.com>
2026-05-09 22:18:09 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类xu
-  HEO_HERO_CATEGORY_1: { title: '팀원소개', url: '/about' },
-  HEO_HERO_CATEGORY_2: { title: '프로젝트보기', url: '/archive' },
-  HEO_HERO_CATEGORY_3: { title: '문의하기', url: '/contact' },
+  HEO_HERO_CATEGORY_1: { title: '사이트소개', url: '/contact' },
+  HEO_HERO_CATEGORY_2: { title: '팀원소개', url: '/about' },
+  HEO_HERO_CATEGORY_3: { title: '프로젝트보기', url: '/archive' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
