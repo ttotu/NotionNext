14a5115ce5637b7ee24b161af0a68dfe888e8e4d
ttotu <58546777+ttotu@users.noreply.github.com>
2026-06-04 18:35:29 +0800

    修改联系方式

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '한중 통역 전문', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '수행통역 | 비지니스통역 | Email: linkasia-info@naver.com | Kakaotalk ID:  | Wechat ID: linkasia-info', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Email: linkasia-info@naver.com     |     KakaoTalk ID: linkasia-info     |     Wechat ID: linkasia-info', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.linkasia.top/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '중국어통역,상하이,중국통역', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
